3e43e1531d2b60a314b20811a99b69ebc2dfa568
Anunay Kulshrestha <anunay@stanford.edu>
2016-10-21 01:52:49 -0700

    More buggy code coming your way, boys

--- main_test.go
+++ main_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"crypto/sha256"
-	// "fmt"
+	"fmt"
 	"math/big"
 	"math/rand"
 	"testing"
@@ -79,56 +79,103 @@ func TestDiscreteLogEquality(test *testing.T) {
 	// randGen := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 	randGen := rand.New(rand.NewSource(420))
 
-	var G []big.Int
+	var x, y1, y2 big.Int
+	g1 := big.NewInt(6525162)
+	g2 := big.NewInt(1160783)
+
+	// var G []big.Int
 	// G = make([]big.t)
 	// G[0] = big.NewInt(6525162)
 	// G[1] = big.NewInt(1160783)
 	// G[2] = big.NewInt(3090959)
-	var x big.Int
+	// var x big.Int
 
 	// Generate private key
 	x.Rand(randGen, q)
 
-	for i := 0; i < 10; i++ {
-		var n big.Int
-		n.Rand(randGen, q)
-		G = append(G, n)
-	}
+	y1.Exp(g1, &x, p)
+	y2.Exp(g2, &x, p)
+
+	fmt.Print("y1 = ")
+	fmt.Println(y1.String())
+	fmt.Print("y2 = ")
+	fmt.Println(y2.String())
+
+	// for i := 0; i < 10; i++ {
+	//   var n big.Int
+	//   n.Rand(randGen, q)
+	//   G = append(G, n)
+	// }
 
 	// fmt.Println("G = ")
 	// fmt.Println(G)
 
 	// Generate zero-knowledge proof
-	t, r := DiscreteLogEquality(x, G, *p, *q)
+	a, b, r := DiscreteLogEquality(x, *g1, *g2, *p, *q)
 
 	// Verification
 	h := sha256.New()
-	var v, c, n big.Int
+	var c big.Int
+
+	h.Write(g1.Bytes()[:])
+	h.Write(g2.Bytes()[:])
+	h.Write(y1.Bytes()[:])
+	h.Write(y2.Bytes()[:])
+	h.Write(a.Bytes()[:])
+	h.Write(b.Bytes()[:])
 
 	// Compute c
-	Y := make([]big.Int, len(G))
-	for i := 0; i < len(G); i++ {
-		Y[i].Exp(&G[i], &x, p)
-		h.Write(G[i].Bytes()[:])
-		h.Write(Y[i].Bytes()[:])
-		h.Write(t[i].Bytes()[:])
-	}
+	// Y := make([]big.Int, len(G))
+	// for i := 0; i < len(G); i++ {
+	//   Y[i].Exp(&G[i], &x, p)
+	//   h.Write(G[i].Bytes()[:])
+	//   h.Write(Y[i].Bytes()[:])
+	//   h.Write(t[i].Bytes()[:])
+	// }
 
 	c.SetBytes(h.Sum(nil))
 	c.Mod(&c, p)
 
-	for i := 0; i < len(G); i++ {
-		// Compute g^r * y^c
-		v.Exp(&G[i], &r, p)
-		n.Exp(&Y[i], &c, p)
-		v.Mul(&v, &n)
-		v.Mod(&v, p)
+	fmt.Print("c = ")
+	fmt.Println(c.String())
 
-		// So what do we have here?
-		if t[i].Cmp(&v) != 0 {
-			test.Error("WRONG! " + G[i].String() + " Expected " + v.String() + ", got " + t[i].String())
-		} else {
-			test.Log("RIGHT! " + G[i].String() + " Expected " + v.String() + ", got " + t[i].String())
-		}
+	y1.Exp(&y1, &c, nil)
+	y2.Exp(&y2, &c, nil)
+
+	y1.Mul(&y1, &a)
+	y2.Mul(&y2, &b)
+
+	y1.Mod(&y1, p)
+	y2.Mod(&y2, p)
+
+	g1.Exp(g1, &r, p)
+	g2.Exp(g2, &r, p)
+
+	// v.Exp(&g1, &r, p)
+	// n.Exp(&Y[i], &c, p)
+	// v.Mul(&v, &n)
+	// v.Mod(&v, p)
+
+	if g1.Cmp(&y1) != 0 {
+		test.Error("WRONG! " + g1.String() + " Expected " + y1.String())
+	} else {
+		test.Log("RIGHT! " + g1.String() + " Expected " + y1.String())
 	}
+
+	if g2.Cmp(&y2) != 0 {
+		test.Error("WRONG! " + g2.String() + " Expected " + y2.String())
+	} else {
+		test.Log("RIGHT! " + g2.String() + " Expected " + y2.String())
+	}
+
+	// for i := 0; i < len(G); i++ {
+	//   // Compute g^r * y^c
+	//   v.Exp(&G[i], &r, p)
+	//   n.Exp(&Y[i], &c, p)
+	//   v.Mul(&v, &n)
+	//   v.Mod(&v, p)
+	//
+	//   // So what do we have here?
+	//
+	// }
 }
--- zkp.go
+++ zkp.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"crypto/sha256"
-	// "fmt"
+	"fmt"
 	"math/big"
 	"math/rand"
 	"time"
@@ -42,44 +42,105 @@ func DiscreteLogKnowledge(x big.Int, g big.Int, p big.Int, q big.Int) (big.Int, 
 // logarithms of k values are equal using the Fiat–Shamir heuristic and
 // returns the proof tuple (t[], r). The total size of the ZKP is k * log p +
 // log q bits.
-func DiscreteLogEquality(x big.Int, g []big.Int, p big.Int, q big.Int) ([]big.Int, big.Int) {
+// func DiscreteLogEquality(x big.Int, g []big.Int, p big.Int, q big.Int) ([]big.Int, big.Int) {
+//   // Setup a hash and PRG
+//   h := sha256.New()
+//   // randGen := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+//   randGen := rand.New(rand.NewSource(690))
+//
+//   var v, c, A, r big.Int
+//
+//   // fmt.Println("G = ")
+//   // fmt.Println(g)
+//
+//   // Compute t
+//   v.Rand(randGen, &q)
+//   t := make([]big.Int, len(g))
+//   Y := make([]big.Int, len(g))
+//
+//   for i := 0; i < len(g); i++ {
+//     t[i].Exp(&g[i], &v, &p)
+//     Y[i].Exp(&g[i], &x, &p)
+//   }
+//
+//   // Compute c
+//   for i := 0; i < len(g); i++ {
+//     h.Write(g[i].Bytes()[:])
+//     h.Write(Y[i].Bytes()[:])
+//     h.Write(t[i].Bytes()[:])
+//   }
+//
+//   c.SetBytes(h.Sum(nil))
+//   c.Mod(&c, &p)
+//
+//   // fmt.Print("c = ")
+//   // fmt.Println(c.String())
+//
+//   // Calculate r = v - cx
+//   A.Mul(&c, &x)
+//   r.Sub(&v, &A)
+//   r.Mod(&r, &q)
+//
+//   return t, r
+// }
+
+func DiscreteLogEquality(x big.Int, g1 big.Int, g2 big.Int, p big.Int, q big.Int) (big.Int, big.Int, big.Int) {
 	// Setup a hash and PRG
 	h := sha256.New()
 	// randGen := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 	randGen := rand.New(rand.NewSource(690))
 
-	var v, c, A, r big.Int
+	var z, c, a, b, y1, y2, r big.Int
+	var temp big.Int
 
 	// fmt.Println("G = ")
 	// fmt.Println(g)
 
 	// Compute t
-	v.Rand(randGen, &q)
-	t := make([]big.Int, len(g))
-	Y := make([]big.Int, len(g))
+	z.Rand(randGen, &q)
 
-	for i := 0; i < len(g); i++ {
-		t[i].Exp(&g[i], &v, &p)
-		Y[i].Exp(&g[i], &x, &p)
-	}
+	// t := make([]big.Int, len(g))
+	// Y := make([]big.Int, len(g))
+	y1.Exp(&g1, &x, &p)
+	y2.Exp(&g2, &x, &p)
+
+	fmt.Print("y1 = ")
+	fmt.Println(y1.String())
+	fmt.Print("y2 = ")
+	fmt.Println(y2.String())
+
+	// for i := 0; i < len(g); i++ {
+	//   t[i].Exp(&g[i], &v, &p)
+	//   Y[i].Exp(&g[i], &x, &p)
+	// }
+	a.Exp(&g1, &z, &p)
+	b.Exp(&g2, &z, &p)
+
+	h.Write(g1.Bytes()[:])
+	h.Write(g2.Bytes()[:])
+	h.Write(y1.Bytes()[:])
+	h.Write(y2.Bytes()[:])
+	h.Write(a.Bytes()[:])
+	h.Write(b.Bytes()[:])
 
 	// Compute c
-	for i := 0; i < len(g); i++ {
-		h.Write(g[i].Bytes()[:])
-		h.Write(Y[i].Bytes()[:])
-		h.Write(t[i].Bytes()[:])
-	}
+	// for i := 0; i < len(g); i++ {
+	//
+	//   h.Write(g[i].Bytes()[:])
+	//   h.Write(Y[i].Bytes()[:])
+	//   h.Write(t[i].Bytes()[:])
+	// }
 
 	c.SetBytes(h.Sum(nil))
 	c.Mod(&c, &p)
 
-	// fmt.Print("c = ")
-	// fmt.Println(c.String())
+	fmt.Print("c = ")
+	fmt.Println(c.String())
 
 	// Calculate r = v - cx
-	A.Mul(&c, &x)
-	r.Sub(&v, &A)
+	temp.Mul(&c, &x)
+	r.Add(&z, &temp)
 	r.Mod(&r, &q)
 
-	return t, r
+	return a, b, r
 }
