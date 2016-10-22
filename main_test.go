a8325e421526e51b9b698392b5f49fa4206f4d7c
Matthew P. Denton <mdenton@cs.stanford.edu>
2016-10-21 18:35:30 -0700

    It is corect and better commented, need to change variable names and delete commented code

--- main_test.go
+++ main_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"crypto/sha256"
-	// "fmt"
+	"fmt"
 	"math/big"
 	"math/rand"
 	"testing"
@@ -10,65 +10,75 @@ import (
 )
 
 func TestDiscreteLogKnowledge(test *testing.T) {
-	// Err.. too small?
+	// TODO Err.. too small?
+	// ZKP constants
 	p := big.NewInt(34531109)
 	q := big.NewInt(8632777)
-	g := big.NewInt(6655320)
+	g := big.NewInt(6655320) // TODO needs to be a generator
 
 	randGen := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 
-	var x, y big.Int
+	for i := 0; i < 50; i++ {
+		var x, y big.Int
 
-	// Generate private key, public key pair
-	x.Rand(randGen, q)
-	y.Exp(g, &x, p)
+		// Generate private key, public key pair
+		x.Rand(randGen, q) // x = rand() mod q // TODO also shouldn't be 0
+		y.Exp(g, &x, p) // y = g^x mod p
 
-	// Generate zero-knowledge proof
-	t, r := DiscreteLogKnowledge(x, *g, *p, *q)
+		// Generate zero-knowledge proof
+		t, r := DiscreteLogKnowledge(x, *g, *p, *q)
 
-	// Verification
-	h := sha256.New()
-	var v, c big.Int
+		// Verification
+		h := sha256.New()
+		var tv, c big.Int
 
-	// Compute c
-	h.Write(g.Bytes()[:])
-	h.Write(y.Bytes()[:])
-	h.Write(t.Bytes()[:])
-	c.SetBytes(h.Sum(nil))
-	c.Mod(&c, p)
+		// Compute c = SHA256(g,y,t)
+		h.Write(g.Bytes()[:])
+		h.Write(y.Bytes()[:])
+		h.Write(t.Bytes()[:])
+		c.SetBytes(h.Sum(nil))
+		c.Mod(&c, q) // c = c mod q
 
-	// Compute g^r * y^c
-	v.Exp(g, &r, p)
-	c.Exp(&y, &c, p)
-	v.Mul(&v, &c)
-	v.Mod(&v, p)
+		// Compute tv = g^r * y^c mod p
+		tv.Exp(g, &r, p)
+		c.Exp(&y, &c, p)
+		tv.Mul(&tv, &c)
+		tv.Mod(&tv, p)
 
-	// So what do we have here?
-	if t.Cmp(&v) != 0 {
-		test.Error("Expected " + v.String() + ", got " + t.String())
+		// So what do we have here?
+		if t.Cmp(&tv) != 0 {
+			test.Error("Expected " + tv.String() + ", got " + t.String())
+		}
 	}
 }
 
-func GenerateG(l int, q big.Int) []big.Int {
+func GenerateG(p *big.Int, q *big.Int, numGs int) []big.Int {
 	var G []big.Int
-	var n, z big.Int
+	var j, pMinusOne, h big.Int
+
+	pMinusOne.Sub(p, one)
+	j.Div(&pMinusOne, q)
 
 	randGen := rand.New(rand.NewSource(42))
 
-	for {
-		n.Rand(randGen, &q)
-		flag := true
-		for j := 0; j < len(G); j++ {
-			z.GCD(nil, nil, &G[j], &n)
-			if z.Cmp(big.NewInt(1)) != 0 {
-				flag = false
+	for i := 0; i < numGs; i++ {
+		for {
+			var temp big.Int
+			G = append(G, temp)
+			// find random number not equal to 1
+			for {
+				h.Rand(randGen, &pMinusOne)
+				if h.Cmp(one) != 0 {
+					break
+				}
+			}
+			G[i].Exp(&h, &j, p) // G[i] = h^j mod p
+			if G[i].Cmp(one) != 0 { // if it's not 1, we are done
 				break
 			}
 		}
-		if flag == true {
-			G = append(G, n)
-		}
 	}
+	return G
 }
 
 func TestDiscreteLogEquality(test *testing.T) {
@@ -76,24 +86,26 @@ func TestDiscreteLogEquality(test *testing.T) {
 	p := big.NewInt(34531109)
 	q := big.NewInt(8632777)
 
-	// randGen := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
-	randGen := rand.New(rand.NewSource(420))
+	randGen := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
+	// randGen := rand.New(rand.NewSource(420))
 
-	var G []big.Int
 	// G = make([]big.t)
 	// G[0] = big.NewInt(6525162)
 	// G[1] = big.NewInt(1160783)
 	// G[2] = big.NewInt(3090959)
-	var x big.Int
 
 	// Generate private key
+	var x big.Int
 	x.Rand(randGen, q)
 
-	for i := 0; i < 10; i++ {
-		var n big.Int
-		n.Rand(randGen, q)
-		G = append(G, n)
-	}
+	// Generate all the g's (bases of the exponentiation)
+	// for i := 0; i < 10; i++ {
+	// 	var n big.Int
+	// 	n.Rand(randGen, q)
+	// 	G = append(G, n)
+	// }
+
+	G := GenerateG(p, q, 10)
 
 	// fmt.Println("G = ")
 	// fmt.Println(G)
@@ -103,7 +115,7 @@ func TestDiscreteLogEquality(test *testing.T) {
 
 	// Verification
 	h := sha256.New()
-	var v, c, n big.Int
+	var tv, c, n big.Int
 
 	// Compute c
 	Y := make([]big.Int, len(G))
@@ -115,20 +127,23 @@ func TestDiscreteLogEquality(test *testing.T) {
 	}
 
 	c.SetBytes(h.Sum(nil))
-	c.Mod(&c, p)
+	c.Mod(&c, q) // c = c mod q
+
+	fmt.Print("c = ")
+	fmt.Println(c.String())
 
 	for i := 0; i < len(G); i++ {
-		// Compute g^r * y^c
-		v.Exp(&G[i], &r, p)
+		// Compute tv = g^r * y^c mod p
+		tv.Exp(&G[i], &r, p)
 		n.Exp(&Y[i], &c, p)
-		v.Mul(&v, &n)
-		v.Mod(&v, p)
+		tv.Mul(&tv, &n)
+		tv.Mod(&tv, p)
 
 		// So what do we have here?
-		if t[i].Cmp(&v) != 0 {
-			test.Error("WRONG! " + G[i].String() + " Expected " + v.String() + ", got " + t[i].String())
+		if t[i].Cmp(&tv) != 0 {
+			test.Error("WRONG! " + G[i].String() + " Expected " + tv.String() + ", got " + t[i].String())
 		} else {
-			test.Log("RIGHT! " + G[i].String() + " Expected " + v.String() + ", got " + t[i].String())
+			test.Log("RIGHT! " + G[i].String() + " Expected " + tv.String() + ", got " + t[i].String())
 		}
 	}
 }
--- zkp.go
+++ zkp.go
@@ -2,38 +2,47 @@ package main
 
 import (
 	"crypto/sha256"
-	// "fmt"
+	"fmt"
 	"math/big"
 	"math/rand"
 	"time"
 )
 
+/*
+ * Variable names follow THIS WIKIPEDIA ARTICLE:
+ * https://en.wikipedia.org/wiki/Fiat%E2%80%93Shamir_heuristic
+ */
+
 // DiscreteLogKnowledge generates a ZKP of the knowledge of a discrete
 // logarithm using the Fiat–Shamir heuristic and returns the proof pair
 // (t, r). The total size of the ZKP is log p + log q bits.
 func DiscreteLogKnowledge(x big.Int, g big.Int, p big.Int, q big.Int) (big.Int, big.Int) {
-	// Setup a hash and PRG
-	h := sha256.New()
+	h := sha256.New() // h can be used to calculate the sha256
+
+	// Set up random number generator
 	randGen := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 
+	// A is a temporary variable for below
 	var v, t, c, r, A, y big.Int
-	y.Exp(&g, &x, &p)
+
+	// Calculate public key from private key
+	y.Exp(&g, &x, &p) // y = g^x mod p
 
 	// Compute t
-	v.Rand(randGen, &q)
-	t.Exp(&g, &v, &p)
+	v.Rand(randGen, &q) // v = rand() mod q
+	t.Exp(&g, &v, &p) // t = g^v mod p
 
-	// Compute c
+	// Compute c = SHA256(g,y,t)
 	h.Write(g.Bytes()[:])
 	h.Write(y.Bytes()[:])
 	h.Write(t.Bytes()[:])
 	c.SetBytes(h.Sum(nil))
-	c.Mod(&c, &p)
+	c.Mod(&c, &q) // c = c mod q
 
 	// Calculate r = v - cx
-	A.Mul(&c, &x)
-	r.Sub(&v, &A)
-	r.Mod(&r, &q)
+	A.Mul(&c, &x) // A = c * x
+	r.Sub(&v, &A) // r = v - c*x
+	r.Mod(&r, &q) // r = r mod q
 
 	return t, r
 }
@@ -43,8 +52,9 @@ func DiscreteLogKnowledge(x big.Int, g big.Int, p big.Int, q big.Int) (big.Int, 
 // returns the proof tuple (t[], r). The total size of the ZKP is k * log p +
 // log q bits.
 func DiscreteLogEquality(x big.Int, g []big.Int, p big.Int, q big.Int) ([]big.Int, big.Int) {
-	// Setup a hash and PRG
-	h := sha256.New()
+	h := sha256.New() // h can be used to calculate the sha256
+
+	// Set up random number generator
 	// randGen := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 	randGen := rand.New(rand.NewSource(690))
 
@@ -59,11 +69,11 @@ func DiscreteLogEquality(x big.Int, g []big.Int, p big.Int, q big.Int) ([]big.In
 	Y := make([]big.Int, len(g))
 
 	for i := 0; i < len(g); i++ {
-		t[i].Exp(&g[i], &v, &p)
-		Y[i].Exp(&g[i], &x, &p)
+		t[i].Exp(&g[i], &v, &p) // t[i] = g[i]^v mod p
+		Y[i].Exp(&g[i], &x, &p) // Y[i] = g[i]^x mod p
 	}
 
-	// Compute c
+	// Compute c = H(g[i], Y[i], t[i])
 	for i := 0; i < len(g); i++ {
 		h.Write(g[i].Bytes()[:])
 		h.Write(Y[i].Bytes()[:])
@@ -71,15 +81,20 @@ func DiscreteLogEquality(x big.Int, g []big.Int, p big.Int, q big.Int) ([]big.In
 	}
 
 	c.SetBytes(h.Sum(nil))
-	c.Mod(&c, &p)
+	c.Mod(&c, &q) // c = c mod q
 
-	// fmt.Print("c = ")
-	// fmt.Println(c.String())
+	fmt.Print("c = ")
+	fmt.Println(c.String())
 
-	// Calculate r = v - cx
-	A.Mul(&c, &x)
-	r.Sub(&v, &A)
-	r.Mod(&r, &q)
+	// Calculate r = v - cx mod q
+	A.Mul(&c, &x) // A = c*x
+	r.Sub(&v, &A) // r = v - c*x
+	r.Mod(&r, &q) // r = r mod q
+
+	// one := big.NewInt(1)
+	// var pMinusOne big.Int
+	// pMinusOne.Sub(&p, one)
+	// r.Mod(&r, &pMinusOne)
 
 	return t, r
 }
